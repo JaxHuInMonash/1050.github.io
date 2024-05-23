1130eb8daf87d742c20a2d2f9281223b1fecc81b
JaxHuInMonash <hhuu0039@student.monash.edu>
2024-05-23 20:22:41 +1000

    Delete faq-question.js

--- faq-question.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(item => {
-    item.addEventListener('click', event => {
-        const faqItem = event.target.parentElement;
-        faqItem.classList.toggle('active');
-    });
-});
